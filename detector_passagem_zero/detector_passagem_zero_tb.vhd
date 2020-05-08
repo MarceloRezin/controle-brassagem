18231b1f0b43bd642b70bf49c9be74984f774c2a
Marcelo Rezin <marcelo.rezin@gmail.com>
2020-05-08 16:33:35 -0300

    Controle de potencia

--- detector_passagem_zero/detector_passagem_zero_tb.vhd
+++ detector_passagem_zero/detector_passagem_zero_tb.vhd
@@ -12,21 +12,36 @@ end detector_passagem_zero_tb;
 
 architecture main of detector_passagem_zero_tb is
 
+    signal  clk                 :   std_logic                       :=  '0';
     signal  passagem_zero       :   std_logic                       :=  '0';
+    signal  centena             :   std_logic_vector(6 downto 0);
+    signal  dezena              :   std_logic_vector(6 downto 0);
+    signal  unidade             :   std_logic_vector(6 downto 0);
     signal  saida               :   std_logic                       :=  '0';
 
     signal  cout_pass           : integer   :=  0;
+    signal  cout_saida          : integer   :=  0;
 
 begin
 
+    clk <=  not clk after 10 ns;
+
     passagem_zero   <=  not passagem_zero after 4.16666667 ms;
 
-    DUT :   entity work.detector_passagem_zero(main) port map(passagem_zero, saida);
+    DUT :   entity work.detector_passagem_zero(main) port map(clk, passagem_zero, centena, dezena, unidade, saida);
+
+    process(passagem_zero)
+    begin
+        if rising_edge(passagem_zero) then
+            cout_pass   <= cout_pass + 1;
+        end if;
+
+    end process;
 
     process(saida)
     begin
         if rising_edge(saida) then
-            cout_pass   <= cout_pass + 1;
+            cout_saida   <= cout_saida + 1;
         end if;
 
     end process;
