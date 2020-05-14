8b2fc18e506793be576689de4a92779a079b0aa0
Marcelo Rezin <marcelo.rezin@gmail.com>
2020-05-14 16:50:22 -0300

    Documentação e ajuste das funções

--- memoria_rampa/memoria_rampa.vhd
+++ memoria_rampa/memoria_rampa.vhd
@@ -1,6 +1,9 @@
 -- Módulo de memória para armazenamento das rampas de temperatura que serão utilizadas na receita 
--- 0 -> temperatura
--- 1 -> tempo
+-- O tipo 'parametros' pode armazenar tanto temperatura alvo quanto o tempo. A temperatura é sempre 
+-- em graus celsius com o valor máximo de 1023,75°C (A capacidade real é 4093, porém é dividido por 4 devido ao ponto fixo).
+-- Já o tempo é armazenado em segundos e pode atingir no máximo 8191 s, o que resulta em um tempo de 2h:16m:31s. 
+-- O primeiro parâmetro (posição 0) é sempre a temperatura e o segundo (posição 1) é sempre o tempo.
+-- O tipo 'rampa' armazena todos os paramêtros das rampas de uma receita. Pode conter no máximo 9 temperaturas;
 -- Autor: Marcelo Rezin
 -- Data: 14/05/2020
 
@@ -12,24 +15,18 @@ package memoria_rampa is
     type    parametros  is array (0 to 1)   of std_logic_vector(12 downto 0);
     type    rampa       is array (0 to 9)   of parametros;
 
-    function    get_rampa(i : integer; rp : rampa)  return  rampa;
     function    get_temperatura(p : parametros)     return  std_logic_vector(12 downto 0);
     function    get_tempo(p : parametros)           return  std_logic_vector(12 downto 0);
 end;
 
 package body memoria_rampa is
 
-    function get_parametros(i : integer; rp : rampa)  return  parametros is
-    begin
-        return  rp(i);
-    end function;
-
     function get_temperatura(p : parametros)   return std_logic_vector(12 downto 0) is
     begin
         return p(0);
     end function;
 
-    function get_temperatura(p : parametros)   return std_logic_vector(12 downto 0) is
+    function get_tempo(p : parametros)   return std_logic_vector(12 downto 0) is
     begin
         return p(1);
     end function;
