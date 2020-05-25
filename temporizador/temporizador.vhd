523c1446424d503d739f4744c7fa04c1fd06e5f8
Marcelo Rezin <marcelo.rezin@gmail.com>
2020-05-25 11:24:00 -0300

    Ajuste no tempo de inicialização do temporizador

--- temporizador/temporizador.vhd
+++ temporizador/temporizador.vhd
@@ -34,18 +34,6 @@ architecture main of temporizador is
         return get_temperatura(rampas(index))(11 downto 0);
     end set_set_point;
 
-    component divisor_clock is
-        port(
-            clk     :   in  std_logic;
-            divisor :   in  integer range 0 to 50000000 :=  0; -- divisão máxima de 1hz (De acordo com a placa utilizada). Dá pra usar divisores em cascata  
-            
-            out_clk :   out std_logic
-        );
-    end component divisor_clock;
-
-    constant    prescaler               :   integer     :=  1_000_000;
-    signal      clk_1HZ                 :   std_logic   :=  '0'; -- Clock de 1 segundo para contagem do tempo
-
     signal      alteracao_set_point_tmp :   std_logic   :=  '0';
     signal      fim_tmp                 :   std_logic   :=  '0';
 
@@ -54,14 +42,13 @@ architecture main of temporizador is
 
 begin
 
-    divisor_1Mx :   divisor_clock port map(clk_1MHZ, prescaler, clk_1HZ);
-
     alteracao_set_point <=  alteracao_set_point_tmp;
     fim                 <=  fim_tmp;
     reset_iniciado      <=  fim_tmp or reset;
 
-    process(clk_1HZ, iniciar)
+    process(clk_1MHZ)
         
+        variable    delay_1s            :   integer range 0 to 1_000_000;
         variable    segundo_zero        :   std_logic :=  '1';
         variable    index               :   integer range 0 to 9; --Indica qual rampa que está sendo executada no momento 
         variable    tempo_alvo          :   integer range 0 to 8_191;
@@ -69,8 +56,8 @@ begin
 
     begin
 
-        if rising_edge(clk_1HZ) then
-            
+        if rising_edge(clk_1MHZ) then
+
             if alteracao_set_point_tmp =  '1' then
                 alteracao_set_point_tmp <=  '0';
             end if;
@@ -83,39 +70,48 @@ begin
                 if segundo_zero = '1' then
                     segundo_zero    := '0';
 
+                    delay_1s        :=  0;
                     index           :=  0;
                     tempo_decorrido :=  0;
         
                     tempo_alvo      :=  set_tempo_alvo(index, rampas);
                     set_point       <=  set_set_point(index, rampas);
                 else
-                    tempo_decorrido :=  tempo_decorrido + 1;
+                    delay_1s    :=  delay_1s + 1;
+                    if delay_1s = 1_000_000 then
+                        delay_1s    :=  0;
 
-                    if tempo_decorrido = tempo_alvo then
-                        tempo_decorrido :=  0;
+                        tempo_decorrido :=  tempo_decorrido + 1;
 
-                        if index = 9 then -- acabou
-                            fim_tmp         <=  '1';
-                        else
-                            index       :=  index + 1;
-                            tempo_alvo  :=  set_tempo_alvo(index, rampas);
-                            
-                            if tempo_alvo = 0 then -- acabou
+                        if tempo_decorrido = tempo_alvo then
+                            tempo_decorrido :=  0;
+
+                            if index = 9 then -- acabou
                                 fim_tmp         <=  '1';
-                            else --Trocou o set point
-                                set_point               <=  set_set_point(index, rampas);
-                                alteracao_set_point_tmp <=  '1';
+                            else
+                                index       :=  index + 1;
+                                tempo_alvo  :=  set_tempo_alvo(index, rampas);
+                                
+                                if tempo_alvo = 0 then -- acabou
+                                    fim_tmp         <=  '1';
+                                else --Trocou o set point
+                                    set_point               <=  set_set_point(index, rampas);
+                                    alteracao_set_point_tmp <=  '1';
+                                end if;
                             end if;
+
                         end if;
 
-                    end if;
+                    end if;    
 
                 end if;
 
                 rampa_atual     <=  index;
                 tempo_passado   <= tempo_decorrido;
             else
-                segundo_zero    := '1';
+                if segundo_zero = '0' then
+                    segundo_zero    :=  '1';
+                end if;
             end if;
 
         end if;
